8487e7e7e359209cb9ea5539daf5c3196ca04f37
Zan Loy <zan.loy@gmail.com>
2015-04-07 14:39:13 -0400

    google auth working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resources :log
+
   root 'log#index'
-  
-  get 'log/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
