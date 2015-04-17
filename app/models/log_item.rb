3d549a24079db9431b425d2ecdd4bbe5d3ca92a4
Zan Loy <zan.loy@gmail.com>
2015-04-17 18:34:39 -0400

    added icon for descriptions in logitems list

--- app/models/log_item.rb
+++ app/models/log_item.rb
@@ -8,6 +8,14 @@ class LogItem < ActiveRecord::Base
 
   scope :timeline, -> { order(when: :desc) }
 
+  def has_description?
+    if self.description.nil? or self.description.empty?
+      return false
+    else
+      return true
+    end
+  end
+
   def all_tags=(names)
     self.tags = names.split(',').map do |name|
       Tag.where(name: name.strip).first_or_create!
