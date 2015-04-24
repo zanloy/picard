ca4045400b46e9544a4ad01694416f29be0ea778
Zan Loy <zan.loy@gmail.com>
2015-04-24 08:09:44 -0400

    added apikey field

--- db/migrate/20150420172820_create_profiles.rb
+++ db/migrate/20150420172820_create_profiles.rb
@@ -2,6 +2,7 @@ class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
       t.belongs_to :user, index: true
+      t.string :apikey
       t.string :address
       t.string :company
       t.string :phone
