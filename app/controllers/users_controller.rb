ff49f0f160338676969b6b2a6c8e91ab7d00f4be
Zan Loy <zan.loy@gmail.com>
2015-04-30 13:06:49 -0400

    fixed form-horizontal issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   skip_before_filter :require_login, only: [:new, :create]
+  before_filter :current_user, only: [:new, :create]
 
   before_filter :set_user, only: [:show, :edit, :update, :destroy, :generate_apikey]
   before_filter :require_admin_or_self, only: [:edit, :destroy, :update, :generate_apikey]
