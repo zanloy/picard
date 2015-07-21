031e6735c222dcce786778a67460f78d5fe491d6
Zan Loy <zan.loy@gmail.com>
2015-07-21 16:46:43 -0400

    added new function to cap deploy:invoke

--- config/deploy.rb
+++ config/deploy.rb
@@ -105,4 +105,20 @@ namespace :deploy do
     end
   end
 
+  # ....
+  # @example
+  #   bundle exec cap uat deploy:invoke task=users:update_defaults
+  desc 'Invoke rake task on the server'
+  task :invoke do
+    fail 'no task provided' unless ENV['task']
+
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, ENV['task']
+        end
+      end
+    end
+  end
+
 end
