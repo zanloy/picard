e32cfee0ab395c9b53d067a1305778a6fa33ac25
Zan Loy <zan.loy@gmail.com>
2015-08-03 08:17:14 -0400

    tweak to factory

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [![Build Status](https://travis-ci.org/zanloy/picard.svg)](https://travis-ci.org/zanloy/picard)
+[![Coverage Status](https://coveralls.io/repos/zanloy/picard/badge.svg?branch=master&service=github)](https://coveralls.io/github/zanloy/picard?branch=master)
--- spec/factories/list_items.rb
+++ spec/factories/list_items.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :list_item do
     list
-    payload { JSON.generate({ cn: FFaker::Internet.http_url, expires_on: Time.now.to_s })}
+    payload { JSON.generate({ cn: FFaker::Internet.http_url, expires_on: Date.today })}
     association :updated_by, factory: :user
   end
 end
