d46b5d58b00896d95985db865f6013488b8f2f1d
Zan Loy <zan.loy@gmail.com>
2015-08-28 13:26:37 -0400

    fixes for demo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+
+  if Rails.env.demo?
+    full_host = 'http://demo.zanloy.com/picard'
+  end
+
   provider :google_oauth2,
     ENV['GOOGLE_CLIENT_ID'],
     ENV['GOOGLE_SECRET'],
     { hd: 'sparcedge.com' }
 
-  if Rails.env.demo?
-    provider :google_oauth2, callback_path: '/picard/auth/google_oauth2/callback'
-  end
 end
