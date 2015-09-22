91ceedc9c3e2163fb6b1a1f0ee708df438431fec
Zan Loy <zan.loy@gmail.com>
2015-09-22 19:18:25 -0400

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826115809) do
+ActiveRecord::Schema.define(version: 20150922114709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -227,16 +227,20 @@ ActiveRecord::Schema.define(version: 20150826115809) do
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
-    t.string   "email",                            null: false
+    t.string   "email",                               null: false
     t.string   "hashed_password"
-    t.string   "name",             default: ""
-    t.boolean  "enabled",          default: false
-    t.boolean  "admin",            default: false
+    t.string   "name",                default: ""
+    t.boolean  "enabled",             default: false
+    t.boolean  "admin",               default: false
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
-    t.boolean  "banned",           default: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "banned",              default: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
