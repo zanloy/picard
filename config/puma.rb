009afef5d74e4cc296530decf447fbb882fbaaeb
Zan Loy <zan.loy@gmail.com>
2017-03-14 22:46:54 -0400

    configure puma to use a socket

--- config/puma.rb
+++ config/puma.rb
@@ -15,6 +15,10 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+shared_dir = "#{app_dir}/shared"
+
+bind "#{shared_dir}/sockets/puma.sock"
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
